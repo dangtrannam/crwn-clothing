ca1c9b054a5f98394394dea7e0162d3727fc53f6
dangtrannam <dangtrannam2001@gmail.com>
2021-08-23 14:16:32 +0700

    collection preview, collection item for shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+import SHOP_DATA from "./shop.data";
+
+function ShopPage() {
+  const [collections, setCollections] = useState(SHOP_DATA);
+
+  return (
+    <div className="shop-page">
+      {collections
+        .map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+    </div>
+  );
+}
+
+export default ShopPage;
