8374b004d0c0ce8e16e2cf01888b628cdf574d63
dangtrannam <dangtrannam2001@gmail.com>
2021-08-27 22:49:11 +0700

    Storing userData in our App

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
-  
+
   useEffect(() => {
-    auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user)
+    const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data()
+          });
+        });
+      } else {
+        setCurrentUser(null);
+      }
     });
-  }, [currentUser]);
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className="App">
-      <Header currentUser={currentUser}/>
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
