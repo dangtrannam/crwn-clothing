68846fc9f471edec4c4b9e8190fe3b3a2ddc90fd
dangtrannam <dangtrannam2001@gmail.com>
2021-08-29 01:54:06 +0700

    SignIn with email & pwd

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,16 +3,22 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setEmail("");
-    setPassword("");
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail("");
+      setPassword("");
+
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleChange = (e) => {
@@ -48,7 +54,7 @@ function SignIn() {
           <CustomButton type="submit" value="Submit">
             sign in
           </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
             sign in with google
           </CustomButton>
         </div>
